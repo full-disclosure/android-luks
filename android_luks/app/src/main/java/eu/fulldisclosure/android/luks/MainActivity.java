60480966e2fadd8aeede03a3841e4936b34d9fb6
Full Disclosure <full-disclosure@users.noreply.github.com>
2023-08-31 11:13:04 +0300

    Android studio empty project skeleton

---
+++ android_luks/app/src/main/java/eu/fulldisclosure/android/luks/MainActivity.java
@@ -0,0 +1,15 @@
+package eu.fulldisclosure.android.luks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
