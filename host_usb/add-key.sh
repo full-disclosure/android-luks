c4e2c2bbd88947923937a1c16ff9f321188cfbe2
Full Disclosure <full-disclosure@users.noreply.github.com>
2023-09-03 21:39:47 +0300

    Added scripts for host installation

---
+++ host_usb/add-key.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "Usage: $0 /dev/partition_name"
+	echo "Example: $0 /dev/nvme0n1p5"
+	exit 1
+fi
+
+cryptsetup luksDump "$1"
+res=$?
+
+if [ "$res" -ne "0" ]
+then
+	echo "Could't find a valid LUKS partition or access was denied"
+	exit 2
+fi
+
+echo "Transferring the key from the phone"
+KEY=`./android_luks | xxd -p -c 8192`
+size=${#KEY}
+if [ "$size" -ne "16384" ]
+then
+	echo "Coulnd't get the key. Please unplug the phone, kill the app and retry"
+	exit 3
+fi
+
+echo "Adding the key to UKS partition"
+echo $KEY | xxd -r -p > /root/android_luks_key
+cryptsetup luksAddKey "$1" /root/android_luks_key
+shred /root/android_luks_key
+rm /root/android_luks_key
+
+echo "Done"
+
--- host_usb/install.sh
+++ host_usb/install.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+echo "Install libusb..."
+apt-get install libusb-1.0.0-dev
+echo "Compile..."
+./compile.sh
+
+echo "Copy files to initramfs..."
+cp android_luks       /usr/local/sbin/
 cp openluksdevices.sh /usr/local/sbin/
 cp android_luks       /usr/local/sbin/
+cp crypto.hook        /etc/initramfs-tools/hooks/
+
+echo "Rebuild initramfs..."
+update-initramfs -u -v | grep luks
+
+echo "Done"
 
